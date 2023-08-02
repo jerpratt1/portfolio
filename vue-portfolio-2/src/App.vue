0628459c4026e238ff3207731b6b6cc185dda638
Jeremy Pratt <jerpratt1@gmail.com>
2023-08-02 15:50:11 -0400

    Basic build

--- vue-portfolio-2/src/App.vue
+++ vue-portfolio-2/src/App.vue
@@ -1,20 +1,21 @@
 <template>
   <div id="app">
-    <h2>Jeremy Pratt</h2>
-    
+    <div id="container">
+      <h2 id="title">Jeremy Pratt</h2>
+      <ul id="Info">
+        <li><h2>Info</h2></li>
+        <li> <router-link to="/">Home</router-link> </li>
+        <li> <router-link to="/about">About</router-link> </li>
+      </ul>
+      <ul id="projects">
+        <li><h2>Projects</h2></li>
+        <li>Project name 1</li>
+        <li>Project name 2</li>
+      </ul>
+      <router-view id="box"/>
+    </div>
   </div>
 </template>
-
-<script>
-
-
-export default {
-  name: 'App',
-  components: {
-   
-  }
-}
-</script>
 
 <style>
 #app {
@@ -22,7 +23,56 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: gray;
+}
+
+body{
+  background: rgb(54, 54, 54);
+}
+
+#container{
+  display: grid;
+  grid-template-areas: 
+  "title title title title"
+  "box box info projects"
+  ;
+  column-width: 50%;
+  height: 90svh;
+  width: 90vw;
+  margin: auto;
+  border: #2c3e50 solid 3px;
+}
+
+#title{
+  grid-area: title;
+  /* border: gray 3px solid; */
+  height: 30vh;
+  width: 90vw;
+}
+
+#info{
+  grid-area: info;
+  /* border: gray 3px solid; */
+  width: 20vw;
+  height: 50vh;
+}
+
+#projects{
+  grid-area: projects;
+  /* border: gray 3px solid; */
+  width: 20vw;
+  height: 50vh;
+}
+
+#box{
+  grid-area: box;
+  /* border: gray 3px solid; */
+  width: 40vw;
+  height: 50vh;
+}
+
+ul{
+  display: block;
+  list-style: none;
 }
 </style>
---
+++ vue-portfolio-2/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h2>Welcome.</h2>
+    <p>My name is Jeremy Pratt. I am a full-stack developer based in Columbus, OH, USA. I have developed multiple full stack and front end application. From an educational companion chat bot, to a recipe box application</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
